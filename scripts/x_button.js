615641971689a5ff481e42db9c6c0aa0c4c6c52c
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-11-25 14:05:47 -0500

    Added signup form

---
+++ scripts/x_button.js
@@ -0,0 +1,6 @@
+function closeSignupPopup() {
+  let popup = document.getElementById("header-interest-form");
+  popup.style.display = "none";
+  let x_button = document.getElementById("x-button");
+  x_button.style.display = "none";
+}
