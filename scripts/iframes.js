059c8df5d632d85e7b7c63ced2f7f315fb0a6c7c
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-11-28 22:41:24 -0500

    Finished the website.

--- scripts/iframes.js
+++ scripts/iframes.js
@@ -1,7 +1,11 @@
 function resizeIFrameToFitContent(iFrame) {
   let videosDiv = document.getElementById("videos-div");
   iFrame.width = videosDiv.getBoundingClientRect().width;
-  iFrame.height = videosDiv.getBoundingClientRect().width * (9 / 30);
+  if (window.getComputedStyle(videosDiv).display === "flex") {
+    iFrame.height = videosDiv.getBoundingClientRect().width * (9 / 30);
+  } else {
+    iFrame.height = videosDiv.getBoundingClientRect().width * (18 / 30);
+  }
 }
 
 window.addEventListener('DOMContentLoaded', function (e) {
